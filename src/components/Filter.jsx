676919140f7b99880d237d8193495b0bf55853ca
Rohan <rshirsalkar5@gmail.com>
2022-04-20 19:00:15 +0530

    minor changes saved

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -4,19 +4,17 @@ import List_Item from './List_Item'
 
 export default function Filter(props) {
 
+  let allCitys = props.rideData && props.rideData.map(element => {
+    return(
+      <List_Item handleFilter={props.handleFilter} locationType={"city"} locationName={element.city}  />
+    )
+  })
+
   let allStates = props.rideData && props.rideData.map(element => {
     return (
       <List_Item handleFilter={props.handleFilter} locationType={"state"} locationName={element.state}  />
     )
   })
-
-  // let stateCitys = props.rideData && props.rideData.map(element => {
-  //   return (
-  //     element.state == props.selectedState ? element.city : "no no"
-  //   )
-  // })
-
-  // console.log(props.selectedState)
 
   let stateCitys = []
   props.rideData && props.rideData.forEach(element => {
@@ -43,9 +41,6 @@ export default function Filter(props) {
                   Select State
                 </button>
                 <ul className="dropdown-menu overflow-auto" aria-labelledby="dropdownMenuButton1"  style={{height : "200px"}}>
-                  {/* <li><a className="dropdown-item" onClick={props.handleFilter} id='state' href="#">Tamil Nadu</a></li>
-                  <li><a className="dropdown-item" onClick={props.handleFilter} id='state' href="#">Rajasthan</a></li>
-                  <li><a className="dropdown-item" onClick={props.handleFilter} id='state' href="#">Telangana</a></li> */}
                   {allStates}
                 </ul>
               </div>
@@ -53,12 +48,8 @@ export default function Filter(props) {
                 <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false" style={{ width: "120px" }}>
                   Select City
                 </button>
-                <ul className="dropdown-menu " aria-labelledby="dropdownMenuButton2" >
-                  {/* <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
-                  <List_Item handleFilter={props.handleFilter} location={"city"} locationName={"Place with no name"}  /> */}
-                  {stateCitys}
+                <ul className="dropdown-menu overflow-auto" aria-labelledby="dropdownMenuButton2" style={{maxHeight: "200px"}} >
+                  {stateCitys.length == 0 ? allCitys : stateCitys}
                 </ul>
               </div>
             </div>
